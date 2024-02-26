a98bcaf80f7b6ccf6bbf234f6cbefd92ef0a2e03
emanuelhm <miyagawa.emanuel@gmail.com>
2024-02-26 20:59:49 -0300

    InitialCommit

--- Controllers/FuncionarioController.cs
+++ Controllers/FuncionarioController.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using Azure.Data.Tables;
 using Microsoft.AspNetCore.Mvc;
 using TrilhaNetAzureDesafio.Context;
@@ -12,12 +13,14 @@ public class FuncionarioController : ControllerBase
     private readonly RHContext _context;
     private readonly string _connectionString;
     private readonly string _tableName;
+    private readonly IMapper _mapper;
 
-    public FuncionarioController(RHContext context, IConfiguration configuration)
+    public FuncionarioController(RHContext context, IConfiguration configuration, IMapper mapper)
     {
         _context = context;
         _connectionString = configuration.GetValue<string>("ConnectionStrings:SAConnectionString");
         _tableName = configuration.GetValue<string>("ConnectionStrings:AzureTableName");
+        _mapper = mapper;
     }
 
     private TableClient GetTableClient()
@@ -44,11 +47,12 @@ public class FuncionarioController : ControllerBase
     public IActionResult Criar(Funcionario funcionario)
     {
         _context.Funcionarios.Add(funcionario);
+        _context.SaveChanges();
         // TODO: Chamar o método SaveChanges do _context para salvar no Banco SQL
 
         var tableClient = GetTableClient();
         var funcionarioLog = new FuncionarioLog(funcionario, TipoAcao.Inclusao, funcionario.Departamento, Guid.NewGuid().ToString());
-
+        tableClient.UpsertEntity(funcionarioLog);
         // TODO: Chamar o método UpsertEntity para salvar no Azure Table
 
         return CreatedAtAction(nameof(ObterPorId), new { id = funcionario.Id }, funcionario);
@@ -62,16 +66,16 @@ public class FuncionarioController : ControllerBase
         if (funcionarioBanco == null)
             return NotFound();
 
-        funcionarioBanco.Nome = funcionario.Nome;
-        funcionarioBanco.Endereco = funcionario.Endereco;
+        funcionarioBanco = _mapper.Map<Funcionario>(funcionario);
         // TODO: As propriedades estão incompletas
 
         // TODO: Chamar o método de Update do _context.Funcionarios para salvar no Banco SQL
+        _context.Funcionarios.Update(funcionarioBanco);
         _context.SaveChanges();
 
         var tableClient = GetTableClient();
         var funcionarioLog = new FuncionarioLog(funcionarioBanco, TipoAcao.Atualizacao, funcionarioBanco.Departamento, Guid.NewGuid().ToString());
-
+        tableClient.UpsertEntity(funcionarioLog);
         // TODO: Chamar o método UpsertEntity para salvar no Azure Table
 
         return Ok();
@@ -86,11 +90,12 @@ public class FuncionarioController : ControllerBase
             return NotFound();
 
         // TODO: Chamar o método de Remove do _context.Funcionarios para salvar no Banco SQL
+        _context.Funcionarios.Remove(funcionarioBanco);
         _context.SaveChanges();
 
         var tableClient = GetTableClient();
         var funcionarioLog = new FuncionarioLog(funcionarioBanco, TipoAcao.Remocao, funcionarioBanco.Departamento, Guid.NewGuid().ToString());
-
+        tableClient.UpsertEntity(funcionarioLog);
         // TODO: Chamar o método UpsertEntity para salvar no Azure Table
 
         return NoContent();
